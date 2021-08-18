ce2893bc5d7dc06e77d82fdffad94ce1915cfdfd
Daniel Gratz <gratz.dhg@gmail.com>
2021-08-18 14:29:28 -0400

    fixed installer script

--- packages/LongQt/meta/installscript.js
+++ packages/LongQt/meta/installscript.js
@@ -5,11 +5,11 @@ Component.prototype.createOperations = function() {
     component.createOperations();
 
     if (systemInfo.productType === "windows") {
-        component.addOperation("CreateShortcut", "@TargetDir@/bin/LongQt.exe", "@StartMenuDir@/LongQt/LongQt.lnk",
-            "workingDirectory=@TargetDir@/bin", "description=Simulate heart cells");
-        component.addOperation("CreateShortcut", "@TargetDir@/bin/LongQtGrapher.exe", "@StartMenuDir@/LongQt/LongQtGrapher.lnk",
-            "workingDirectory=@TargetDir@/bin", "description=Plot previously run simulations");
-        component.addOperation("CreateShortcut", "@TargetDir@/bin/LQGridEditor.exe", "@StartMenuDir@/LongQt/LQGridEditor.lnk",
-            "workingDirectory=@TargetDir@/bin", "description=Easily setup grid simulations");
+        component.addOperation("CreateShortcut", "@TargetDir@/LongQt.exe", "@StartMenuDir@/LongQt/LongQt.lnk",
+            "workingDirectory=@TargetDir@", "description=Simulate heart cells");
+        component.addOperation("CreateShortcut", "@TargetDir@/LongQtGrapher.exe", "@StartMenuDir@/LongQt/LongQtGrapher.lnk",
+            "workingDirectory=@TargetDir@", "description=Plot previously run simulations");
+        component.addOperation("CreateShortcut", "@TargetDir@/LQGridEditor.exe", "@StartMenuDir@/LongQt/LQGridEditor.lnk",
+            "workingDirectory=@TargetDir@", "description=Easily setup grid simulations");
     }
 }
