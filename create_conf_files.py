17a9458e49b536ff9fc33a44b462e48e60f50626
Daniel Gratz <gratz.dhg@gmail.com>
2021-08-17 22:01:56 -0400

    Removed LongQt-model and Pylongqt added github action

--- create_conf_files.py
+++ create_conf_files.py
@@ -7,17 +7,18 @@ Created on Tue Feb  5 16:54:44 2019
 """
 
 from shutil import copyfile
+import argparse
 
-version = '0.4'
-try:
-    import PyLongQt
-    version = PyLongQt.version.replace('v','')
-except:
-    print('PyLongQt module not found, using version:',version)
-releaseDate = '2018-10-09'
-longqtRoot = 'U:/LongQt'
-longqtModelRoot = 'U:/LongQt-model'
-pylongqtRoot = 'U:/PyLongQt'
+parser = argparse.ArgumentParser(description='Produce xml config files for Qt Installer Creator')
+parser.add_argument('--version', type=str)
+parser.add_argument('--releaseDate', type=str)
+parser.add_argument('--LQRoot', type=str)
+
+args = parser.parse_args()
+#version = '0.4'
+
+#releaseDate = '2018-10-09'
+#longqtRoot = '/home/dgratz/src/LongQt'
 
 class PackageConfig:
     def __init__(self, ty, location, opts):
@@ -41,41 +42,19 @@ class PackageConfig:
         dest = self.location+'/'+self.opts['license_file']
         copyfile(srcRoot+'/LICENSE',dest)
 
-installerConf = PackageConfig('installer', './config', opts = {'version': version})
+installerConf = PackageConfig('installer', './config', opts = {'version':args.version})
 installerConf.writeConf()
 
 longqtConf = PackageConfig('package', './packages/LongQt/meta', opts = {
  'name': 'LongQt',
  'description': 'LongQt Gui Tools',
- 'version': version,
- 'release_date': releaseDate,
+ 'version': args.version,
+ 'release_date': args.releaseDate,
  'license_name': 'License LongQt',
  'license_file': 'LICENSE-LongQt',
  'default': 'true' })
 longqtConf.writeConf()
-longqtConf.copyLicense(longqtRoot)
-
-longqtModelConf = PackageConfig('package', './packages/LongQtmodel/meta', opts = {
- 'name': 'LongQt model',
- 'description': 'LongQt model library',
- 'version': version,
- 'release_date': releaseDate,
- 'license_name': 'License LongQt model',
- 'license_file': 'LICENSE-LongQt-model',
- 'default': 'false' })
-longqtModelConf.writeConf()
-longqtModelConf.copyLicense(longqtModelRoot)
-
-pylongqtConf = PackageConfig('package', './packages/PyLongQt/meta', opts = {
- 'name': 'PyLongQt',
- 'description': 'Python bindings for LongQt-model',
- 'version': version,
- 'release_date': releaseDate,
- 'license_name': 'License PyLongQt',
- 'license_file': 'LICENSE-PyLongQt',
- 'default': 'false' })
-pylongqtConf.writeConf()
-pylongqtConf.copyLicense(pylongqtRoot)
+longqtConf.copyLicense(args.LQRoot)
 
 #if __name__ == '__main__':
 #    print('Files writen and copied successfully')
